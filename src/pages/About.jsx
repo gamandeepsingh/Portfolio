c3a12b9b76d795d648476635d78e8319d9bfc945
TheGhost <116256043+gamandeepsingh@users.noreply.github.com>
2024-07-25 23:51:30 +0530

    chore: Optimize About page layout and add profile picture

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,16 @@
-
+import img from "../assets/me.jpeg";
 const About = () => {
   return (
     <div className="flex max-w-[1080px] min-h-[600px] flex-wrap justify-center items-center max-md:flex-col-reverse max-md:mt-28">
       {/* left */}
-      <div className="w-2/3 max-md:w-5/6 text-white mr-10 mt-16 max-md:mb-32">
+      <div className="w-2/3 max-md:w-5/6 text-white min-md:mr-10 mt-16 max-md:mb-32">
         <h1 className="text-[#fc0099] font-extrabold text-4xl underline py-3 max-md:text-center">ABOUT ME</h1>
         <p className="max-md:text-center max-md:font-bold">
           Hello! I'm <span className="text-[#fc0099] font-bold italic">Gamandeep Singh</span>, a passionate and driven B.Tech student specializing in Computer Science and Information Technology at KIET Group of Institutions, with an expected graduation in March 2026. With a strong academic background, including a CGPA of 9.28 and impressive percentages in my 10th (92.8%) and 12th (95.8%), I have cultivated a solid foundation in software development. Currently, I am interning as a Software Development Engineer at Elanine Creatives, where I design and develop full-stack web applications using the MERN stack, AWS, and DevOps practices. I also serve as a coordinator at INNOGEEKS, leading a vibrant community of over 200 students in technical projects and workshops. My portfolio showcases my projects, including E-Kapda—a personalized e-commerce platform—and a dynamic real estate website. I am excited to explore new challenges and contribute to innovative solutions in the tech industry.
         </p>
       </div>
       {/* right */}
-      <div className="about flex flex-col justify-center items-center h-full">
+      <div className="about flex flex-col justify-center items-center h-full w-screen">
 
         <div className="relative h-full w-full flex items-center justify-center">
           <div className="profileCard_container relative p-10 border-2 border-dashed rounded-full border-spacing-4 border-gray-400/50">
@@ -80,12 +80,13 @@ const About = () => {
             </button>
 
             <button className="profile_item w-[200px] h-[200px] p-1 border-2 rounded-full hover:border-gray-400/50 cursor-pointer transition-all duration-500 z-0">
-              <div className="w-full bg-white h-full flex items-center justify-center p-2 rounded-full active:scale-95 hover:scale-95 object-cover transition-all duration-500">
-                <span className="w-20 h-20 inline-block">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <div className="w-full bg-white h-full flex items-center justify-center p-2 rounded-full active:scale-95 hover:scale-95 object-cover transition-all duration-500 overflow-hidden">
+                {/* <span className="w-20 h-20 inline-block overflow-hidden"> */}
+                  {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path d="M9.99296258,10.5729355 C12.478244,10.5729355 14.4929626,8.55821687 14.4929626,6.0729355 C14.4929626,3.58765413 12.478244,1.5729355 9.99296258,1.5729355 C7.5076812,1.5729355 5.49296258,3.58765413 5.49296258,6.0729355 C5.49296258,8.55821687 7.5076812,10.5729355 9.99296258,10.5729355 Z M10,0 C13.3137085,0 16,2.6862915 16,6 C16,8.20431134 14.8113051,10.1309881 13.0399615,11.173984 C16.7275333,12.2833441 19.4976819,15.3924771 19.9947005,19.2523727 C20.0418583,19.6186047 19.7690435,19.9519836 19.3853517,19.9969955 C19.0016598,20.0420074 18.6523872,19.7816071 18.6052294,19.4153751 C18.0656064,15.2246108 14.4363723,12.0699838 10.034634,12.0699838 C5.6099956,12.0699838 1.93381693,15.231487 1.39476476,19.4154211 C1.34758036,19.7816499 0.998288773,20.0420271 0.614600177,19.9969899 C0.230911582,19.9519526 -0.0418789616,19.6185555 0.00530544566,19.2523267 C0.500630192,15.4077896 3.28612316,12.3043229 6.97954305,11.1838052 C5.19718955,10.1447285 4,8.21217353 4,6 C4,2.6862915 6.6862915,0 10,0 Z" fill="#555"></path>
-                  </svg>
-                </span>
+                  </svg> */}
+                  <img src={img} alt="" className="w-20 h-20 scale-[2.5] object-cover"/>
+                {/* </span> */}
               </div>
             </button>
           </div>
